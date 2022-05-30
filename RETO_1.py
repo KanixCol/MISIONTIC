8cf5ac539fa84ed1aa78b1f9ae8d8b65216c1b71
Oscar Alberto OARC. Riveros Canizales <oscar.riveros@lincecomercial.com>
2022-05-29 22:43:27 -0500

    Modifique funcion

--- RETO_1.py
+++ RETO_1.py
@@ -10,12 +10,13 @@ user= str(input ("Ingrese el usuario: "))   ##input por parte de los usaurios
 cap= int(input ("Ingrese el monto del capital: "))
 time= int(input ("Ingrese la cantidad de tiempo: "))
 
-if (time >2):
+if (time >2): ##Se evalua el tiempo si es mayor a 2 meses ejecuta una funcion definida previamente o si es menor, imprime el mensaje despues de 
+    #realizar las operaciones definididas en las funciones.  
  print ("Para el usuario " +user , "La cantidad de dinero a recibir, según el monto inicial: $" +str(cap),  
-       "para un tiempo de "+str(time), "meses", "es de: "+ str(CDT(user,cap,time)),"%") 
-##Se llama la variable y los datos del input con los cuales realizar la operacion
+       "para un tiempo de "+str(time), "meses", "es de: "+ str(CDT(user,cap,time)+ (cap)),"Pesos") 
+
 else:
  print ("Para el usuario " +user , "La cantidad de dinero a recibir, según el monto inicial: $" +str(cap),  
-       "para un tiempo de "+str(time), "meses", "es de: "+ str(CDT2(user,cap,time)),"% Por tener el CDT menos tiempo") 
+       "para un tiempo de "+str(time), "meses", "es de: "+ str(CDT2(user,cap,time) - (cap)),"Pesos") 
 
  
